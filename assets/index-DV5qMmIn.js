8f1bcfa135a9be3999cf908a5fc317c1da39050c
yangchanghun <yangchanghun@users.noreply.github.com>
2025-07-12 02:33:16 +0000

    deploy: 0799aed5f612a45971498cddd5167e3b01f35cf7

--- assets/index-DV5qMmIn.js
+++ assets/index-DV5qMmIn.js
@@ -85,7 +85,7 @@
         <!-- 브레드크럼 -->
         <nav class="mb-4">
           <div class="flex items-center space-x-2 text-sm text-gray-600">
-            <a href="/" data-link="" class="hover:text-blue-600 transition-colors">홈</a>
+            <a href="/front_6th_chapter1-1" data-link="" class="hover:text-blue-600 transition-colors">홈</a>
             <svg class="w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
             </svg>
@@ -453,7 +453,7 @@
         <div class="max-w-md mx-auto px-4 py-4">
           <div class="flex items-center justify-between">
             <h1 class="text-xl font-bold text-gray-900">
-              <a href="/" data-link="">쇼핑몰</a>
+              <a href="/front_6th_chapter1-1/" data-link="">쇼핑몰</a>
             </h1>
             <div class="flex items-center space-x-2">
               <!-- 장바구니 아이콘 -->
