0799aed5f612a45971498cddd5167e3b01f35cf7
yangchanghun <gory4848@naver.com>
2025-07-12 11:32:45 +0900

    홈 error

--- src/pages/productPage.js
+++ src/pages/productPage.js
@@ -219,7 +219,7 @@ function productContent() {
         <div class="max-w-md mx-auto px-4 py-4">
           <div class="flex items-center justify-between">
             <h1 class="text-xl font-bold text-gray-900">
-              <a href="/" data-link="">쇼핑몰</a>
+              <a href="/front_6th_chapter1-1/" data-link="">쇼핑몰</a>
             </h1>
             <div class="flex items-center space-x-2">
               <!-- 장바구니 아이콘 -->
